4dad4d366a5bd42dbab54341684e0e91679031a2
lolabird85 <lolahr185@gmail.com>
2026-09-14 19:03:22 -0400

    Unity

--- Brick Breaker/Assets/Scripts/Paddle Behavior.cs
+++ Brick Breaker/Assets/Scripts/Paddle Behavior.cs
@@ -6,6 +6,12 @@ public class PaddleBehavior : MonoBehaviour
         
     public KeyCode LeftDirection = KeyCode.LeftArrow;
     public KeyCode RightDirection = KeyCode.RightArrow;
+    public Transform PaddlePosition;
+
+    void Start()
+    {
+        PaddlePosition = GameObject.Find("Paddle").GetComponent<Transform>();
+    }
     
     void Update()
     {
@@ -23,5 +29,22 @@ public class PaddleBehavior : MonoBehaviour
 
         movement *= Time.deltaTime;
         transform.Translate(movement, 0.0f, 0.0f);
+        
+        //I tried to set a boundary for the level using transform
+        //This is my progress so far
+        //I wanted to do an if statement that if the paddle reached a certain position, I'd stop it from moving farther
+        //I couldn't get it to work without making the paddle unable to move completely
+        if (PaddlePosition.position.x >= 5.0f)
+        {
+            if (Input.GetKey(RightDirection))
+            {
+            }
+        }
+        else if (PaddlePosition.position.x <= -5.0f)
+        {
+            if (Input.GetKey(LeftDirection))
+            {
+            }
+        }
     }
 }
